886e9c4224a2ad2114d0eeb7ececc142ec3417f0
Ulrich Eck <ueck@net-labs.de>
2018-03-22 10:02:51 +0100

    fixed refrence name

--- build.py
+++ build.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     builder = ConanMultiPackager()
 
     for version in available_versions:
-        builder.add({}, {}, {}, {}, reference="cmake_installer/%s" % version)
+        builder.add({}, {}, {}, {}, reference="premake_installer/%s" % version)
 
     builder.run()
